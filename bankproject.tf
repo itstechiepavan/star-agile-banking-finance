611649b0d6b9cbd9e49b9e9618b135986b87ca9d
itstechiepavan <pavansky44@gmail.com>
2024-07-16 17:54:54 +0530

    Update bankproject.tf

--- bankproject.tf
+++ bankproject.tf
@@ -9,19 +9,19 @@ terraform {
 
 # Configure the AWS provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-2a"
 }
 
 # Create an EC2 instance
-resource "aws_instance" "Prod-Server" {
-  ami               = "ami-0a0e5d9c7acc336f1"  # Ubuntu AMI ID for us-east-1
+resource "aws_instance" "New-Server" {
+  ami               = "ami-03f0544597f43a91d"  # Ubuntu AMI ID ap-southeast-2a
   instance_type     = "t2.medium"
-  availability_zone = "us-east-1a"
-  key_name          = "newkeypair"  # Replace with your actual key pair name
+  availability_zone = "ap-southeast-2a"
+  key_name          = "project"  # Replace with your actual key pair name
 
-  vpc_security_group_ids = ["sg-0f53436758b026ae5"]  # Security Group ID
+  vpc_security_group_ids = ["sg-0fd0aa0c6b28357f4"]  # Security Group ID
 
   tags = {
-    Name = "Prod-Server"
+    Name = "New-Server"
   }
 }
